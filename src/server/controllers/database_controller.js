7f46c53d7d9b4b8b18e4dff63e2560ff7a77e5a5
ebietanmi <nachurboi@gmail.com>
2026-07-26 15:29:11 +0100

    i changed the database url, added api domain to .env file,  then update api domain on viteConfig

--- src/server/controllers/database_controller.js
+++ src/server/controllers/database_controller.js
@@ -96,14 +96,16 @@ async function ensureDatabaseExists() {
 ensureDatabaseExists()
 
 
-const pool = mysql2.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10
-}).promise()
+// const pool = mysql2.createPool({
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE,
+//     waitForConnections: true,
+//     connectionLimit: 10
+// }).promise();
+
+const pool = mysql2.createPool(process.env.DATABASE_URL).promise()
 
 
 
