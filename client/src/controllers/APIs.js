40ada20d19fe1b314f41bccf07737c8456d8f041
ebietanmi <nachurboi@gmail.com>
2026-07-28 20:24:53 +0100

    no message

--- client/src/controllers/APIs.js
+++ client/src/controllers/APIs.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.BACKEND_API;
 
 export default async function createUserAPI(credentials) {
   const response = await fetch(`${API_URL}/create-user`, {
